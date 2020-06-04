ece737befa711642c295d6f12cd127eb7b5c5c18
Gamygams1234 <gamyburgos@gmail.com>
2020-06-03 22:40:55 -0700

    making the method for making the  tokens

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,17 @@ class Player {
     this.id = id;
     this.color = color;
     this.isTurn = isTurn;
-    this.tokens = [];
+    this.tokens = this.createTokens(21);
+  }
+  createTokens(num) {
+    // this array will be populated
+    const tokens = [];
+
+    for (let i = 0; i < num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+    }
+
+    return tokens;
   }
 }
--- js/Token.js
+++ js/Token.js
@@ -1,5 +1,5 @@
 class Token {
-  constructor(color) {
+  constructor(owner, id) {
     this.owner = owner;
     this.id = `token-${index}-${owner.id}`;
     this.color = color;
