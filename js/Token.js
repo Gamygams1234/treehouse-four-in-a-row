febc2283e4cbfada174ba654fa0091b0b77152fa
Gamygams1234 <gamyburgos@gmail.com>
2020-06-03 22:33:11 -0700

    making the token class

--- js/Token.js
+++ js/Token.js
@@ -1,1 +1,8 @@
-class Token {}
+class Token {
+  constructor(color) {
+    this.owner = owner;
+    this.id = `token-${index}-${owner.id}`;
+    this.color = color;
+    this.dropped = false;
+  }
+}
